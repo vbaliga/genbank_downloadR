0ced50092612e9e68ab1f8db313e6b0dd49456dc
vbaliga <vbaliga87@gmail.com>
2019-11-30 18:07:00 -0800

    clean up code

--- get_GenBank.R
+++ get_GenBank.R
@@ -1,49 +1,16 @@
-###### for downloading protein sequences ######
-library(rentrez)
-
-## import sequence list from a CSV
-IDs <- read.table("./protein_accessions.csv",
-                  quote="\"", stringsAsFactors=FALSE)
-
-## convert to character lists
-as.list(IDs)$V1 -> prot_list
-
-## use entrez_fetch() to get the protein FASTAs
-prot_gen<-entrez_fetch(id=prot_list,
-                       db="protein",rettype = "fasta")
-
-## write to file
-write(prot_gen, file="my_proteins.fasta")
-
-
-
-###### for downloading DNA sequences ######
-library(ape)
-
-## import each sequence list from its CSV file
-## NO "strings as factor"
-coi <- read.table("./COI_BaligaLaw2016.csv",
-                  quote="\"", stringsAsFactors=FALSE)
-
-## convert to character lists
-as.list(coi)$V1 -> coi_list
-
-## use read.GenBank to acquire the sequences
-coi_gen <- read.GenBank(coi_list, species.names = T)
-
-## use species names
-names_coi<-data.frame(species=attr(coi_gen,"species"),accs=names(coi_gen))
-names(coi_gen)<-attr(coi_gen,"species")
-
-## set WD!!
-
-## export each
-write.dna(coi_gen,"renamed_COI.fasta", format="fasta")
+## Code for downloading batches of DNA sequences from GenBank for multiple
+## genes at a time.
+## Written by Vikram B. Baliga
+## Last updated: 2019-11-30
 
 
 ###### now try multigenes ######
 library(tidyverse)
 
+## the imported csv file should have: 
+## column 1 = species names; example file uses "binomial" as column name
+## each successive column represents one gene
+## each row is one species
 accessions_batch <- read_csv("./multigene_accessions_test.csv")
 
 accessions_list <- 
